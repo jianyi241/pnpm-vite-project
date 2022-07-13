7106f543799e638840395af77f61516b450d3c97
CCH <changhao@arcadedevhouse.com.au>
2022-07-13 10:51:29 +0800

    3d

--- src/views/three-dimensional/three-dimensional.vue
+++ src/views/three-dimensional/three-dimensional.vue
@@ -1,11 +1,19 @@
 <template>
     <div class="three-dimensional">
         <div class="book-list">
-            <div class="book">
+            <div
+                v-for="(item, index) in books"
+                :key="index"
+                class="book"
+                :class="{ checked: activeIndex === index }"
+                @click="activeBook(index)"
+            >
                 <div class="book-item book-left"></div>
                 <div class="book-item book-right"></div>
                 <div class="book-item book-back"></div>
-                <div class="book-item book-front"></div>
+                <div class="book-item book-front">
+                    {{ item }}
+                </div>
                 <div class="book-item book-bottom"></div>
                 <div class="book-item book-top"></div>
             </div>
@@ -13,9 +21,18 @@
     </div>
 </template>
 
-<script>
-export default {
-    name: 'ThreeDimensional'
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const activeIndex = ref<number>(-1)
+const books = ref([1, 2, 3, 4])
+
+const activeBook = (idx: number) => {
+    if (idx === activeIndex.value) {
+        activeIndex.value = -1
+        return
+    }
+    activeIndex.value = idx
 }
 </script>
 
@@ -31,14 +48,37 @@ export default {
         display: flex;
         justify-content: center;
         align-items: center;
-        perspective: 600px;
+        perspective: 9999px;
 
         > .book {
             position: relative;
             width: 282px;
             height: 400px;
             transform-style: preserve-3d;
-            transform: rotateY(20deg);
+            transform: rotateY(45deg);
+            cursor: pointer;
+            margin-left: -240px;
+            //transition: transform 0.5s ease-in-out;
+            &:hover {
+            }
+            &.checked {
+                transform: rotateY(0deg) translateX(-40px) translateY(0);
+                z-index: 999;
+                animation: bookChecked 1s ease-in-out;
+            }
+            @keyframes bookChecked {
+                0% {
+                    transform: rotateY(0deg) translateX(0) translateY(0);
+                }
+
+                50% {
+                    transform: rotateY(0deg) translateX(-40px) translateY(50px);
+                }
+
+                100% {
+                    transform: rotateY(0deg) translateX(-40px) translateY(0);
+                }
+            }
             > .book-item {
                 position: absolute;
             }
@@ -46,9 +86,9 @@ export default {
             > .book-left {
                 width: 30px;
                 height: 400px;
-                background: rgba(0, 174, 236, 0.4);
+                background: rgba(0, 128, 0, 0.6);
                 transform-origin: left;
-                transform: rotateY(-77deg) translateZ(0px);
+                transform: rotateY(-90deg) translateZ(0px);
             }
 
             > .book-back {
@@ -63,23 +103,32 @@ export default {
                 height: 400px;
                 background: rgba(66, 133, 244, 0.4);
                 transform-origin: left;
-                transform: rotateY(0deg) translateZ(29px) translateX(6px);
+                transform: rotateY(0) translateZ(30px) translateX(0);
             }
 
             > .book-right {
                 width: 30px;
                 height: 400px;
-                background: rgba(0, 174, 236, 0.4);
-                transform: rotateY(-30deg) translateZ(-163px) translateX(224px);
+                background: rgba(0, 128, 0, 0.6);
+                transform-origin: left;
+                transform: rotateY(90deg) translateZ(282px) translateX(-30px);
             }
 
-            //> .book-top {
-            //    width: 282px;
-            //    height: 30px;
-            //    background: rgba(0, 174, 236, 0.4);
-            //    transform: skewY(8deg) translate(282px, -503px);
-            //    border: 1px solid #00aeec;
-            //}
+            > .book-top {
+                width: 282px;
+                height: 30px;
+                background: rgba(246, 25, 43, 0.7);
+                transform-origin: top;
+                transform: rotateX(90deg);
+            }
+
+            > .book-bottom {
+                width: 282px;
+                height: 30px;
+                background: rgba(246, 25, 43, 0.7);
+                transform-origin: top;
+                transform: rotateX(90deg) translateZ(-400px);
+            }
         }
     }
 }
