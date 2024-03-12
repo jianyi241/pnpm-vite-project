fd28ca3374e57fcf8a4fe30ec57bc5e8fb411eff
jianyi241 <207357466@qq.com>
2024-03-12 17:10:56 +0800

    添加资源文件，仍然没能跑起来。

--- src/views/genshin-animation/genshin-animation.vue
+++ src/views/genshin-animation/genshin-animation.vue
@@ -1,9 +1,18 @@
 <template>
-    <div></div>
+    <div>
+        <spine-player
+            :width="775"
+            :height="916"
+            :manifest="manifest"
+            :scene-data="sceneData"
+            :resolution-ratio="1"
+        ></spine-player>
+    </div>
 </template>
 
 <script setup lang="ts">
-import './genshin-animation.js'
+import { manifest, sceneData } from './index.js'
+import SpinePlayer from '@/views/genshin-animation/spine-player.vue'
 </script>
 
 <style scoped lang="scss"></style>
--- src/views/genshin-animation/index.js
+++ src/views/genshin-animation/index.js
@@ -56,6 +56,305 @@ const spineData = [
     }
 ]
 
+// export const manifest = [
+//     {
+//         visible: false,
+//         loop: true,
+//         scale: 0.85,
+//         posX: 0,
+//         posY: -33,
+//         renderOrder: 0,
+//         manifest: {
+//             atlas: 'https://act-webstatic.hoyoverse.com/puzzle/hk4e/pz_I4DPYNZuP5/resource/puzzle/2024/01/25/94d23911979e01fcfb3116d559fa9e7d_811043593497286.atlas',
+//             img: [
+//                 {
+//                     src: 'https://act-webstatic.hoyoverse.com/puzzle/hk4e/pz_I4DPYNZuP5/resource/puzzle/2024/01/25/1646e8568f897912142fbb11ff119209_6616164618928613898.png',
+//                     id: 'xianyun',
+//                     type: 'image'
+//                 },
+//                 {
+//                     src: 'https://act-webstatic.hoyoverse.com/puzzle/hk4e/pz_I4DPYNZuP5/resource/puzzle/2024/01/25/22340760f155d73e0533f20b7ce097a8_2416571748329165126.png',
+//                     id: 'xianyun_2',
+//                     type: 'image'
+//                 },
+//                 {
+//                     src: 'https://act-webstatic.hoyoverse.com/puzzle/hk4e/pz_I4DPYNZuP5/resource/puzzle/2024/01/25/bd963437c0f9d8b9813f55099fa3f091_8726771848828636652.png',
+//                     id: 'xianyun_3',
+//                     type: 'image'
+//                 }
+//             ],
+//             json: 'https://act-webstatic.hoyoverse.com/puzzle/hk4e/pz_I4DPYNZuP5/resource/puzzle/2024/01/25/cafbae0ceb609fdab11d779dba1eeaca_6333503252351025414.json',
+//             animations: ['animation'],
+//             name: 'xianyun',
+//             skeleton: {
+//                 hash: 'xXwFqmlEpNc',
+//                 spine: '4.0-from-4.1.24',
+//                 x: -764.26,
+//                 y: -1196.93,
+//                 width: 1256.96,
+//                 height: 1678.08,
+//                 images: './images/',
+//                 audio: 'E:/项目/麻薯/原神/闲云'
+//             }
+//         },
+//         maxVert: 8000,
+//         spineName: 'xianyun',
+//         spineId: 'fJIMc',
+//         animation: 'animation'
+//     },
+//     {
+//         visible: false,
+//         loop: true,
+//         scale: 0.8,
+//         posX: 90,
+//         posY: 30,
+//         renderOrder: 0,
+//         manifest: {
+//             atlas: 'https://act-webstatic.hoyoverse.com/puzzle/hk4e/pz_I4DPYNZuP5/resource/puzzle/2024/01/25/875e7c033df606b0032614b9ffb90c64_2786669480337160903.atlas',
+//             img: [
+//                 {
+//                     src: 'https://act-webstatic.hoyoverse.com/puzzle/hk4e/pz_I4DPYNZuP5/resource/puzzle/2024/01/25/65cf18a3b0c5f7358781f423fcabcfdd_8333780849274354687.png',
+//                     id: 'jaming',
+//                     type: 'image'
+//                 },
+//                 {
+//                     src: 'https://act-webstatic.hoyoverse.com/puzzle/hk4e/pz_I4DPYNZuP5/resource/puzzle/2024/01/25/db4316011c0059c3a189e48c894e22c7_5094089132749168677.png',
+//                     id: 'jaming_2',
+//                     type: 'image'
+//                 }
+//             ],
+//             json: 'https://act-webstatic.hoyoverse.com/puzzle/hk4e/pz_I4DPYNZuP5/resource/puzzle/2024/01/25/fea54e608578c70cee1a26563da72323_1235901433879246255.json',
+//             animations: ['animation'],
+//             name: 'jaming',
+//             skeleton: {
+//                 hash: 'oFsZ1fqlqSI',
+//                 spine: '4.0-from-4.1.24',
+//                 x: -590.84,
+//                 y: -973.41,
+//                 width: 990.72,
+//                 height: 1762.23
+//             }
+//         },
+//         maxVert: 6000,
+//         spineName: 'jaming',
+//         spineId: 'GRzTk',
+//         animation: 'animation'
+//     },
+//     {
+//         visible: false,
+//         loop: true,
+//         scale: 0.8,
+//         posX: -50,
+//         posY: 0,
+//         renderOrder: 0,
+//         manifest: {
+//             atlas: 'https://act-webstatic.hoyoverse.com/puzzle/hk4e/pz_I4DPYNZuP5/resource/puzzle/2024/01/25/0f1ac308c8663b60bf92b5fa5c2afbaf_6482737162982245513.atlas',
+//             img: [
+//                 {
+//                     src: 'https://act-webstatic.hoyoverse.com/puzzle/hk4e/pz_I4DPYNZuP5/resource/puzzle/2024/01/25/8737ea58ec0d01c6dcd434c2446fa35c_1313555480738606983.png',
+//                     id: 'xiao',
+//                     type: 'image'
+//                 },
+//                 {
+//                     src: 'https://act-webstatic.hoyoverse.com/puzzle/hk4e/pz_I4DPYNZuP5/resource/puzzle/2024/01/25/62c9d63f67faa7c4aa3856274753df84_2763786742784922396.png',
+//                     id: 'xiao_2',
+//                     type: 'image'
+//                 }
+//             ],
+//             json: 'https://act-webstatic.hoyoverse.com/puzzle/hk4e/pz_I4DPYNZuP5/resource/puzzle/2024/01/25/c0f8ba4e75b0383fb2d297e35078acef_2978991896829203190.json',
+//             animations: ['animation'],
+//             name: 'xiao',
+//             skeleton: {
+//                 hash: '+vXMXW8ph3w',
+//                 spine: '4.0-from-4.1.24',
+//                 x: -746.91,
+//                 y: -716.87,
+//                 width: 1487.25,
+//                 height: 1872.27
+//             }
+//         },
+//         maxVert: 6000,
+//         spineName: 'xiao',
+//         spineId: '6GC5w',
+//         animation: 'animation'
+//     },
+//     {
+//         visible: false,
+//         loop: true,
+//         scale: 0.9,
+//         posX: 0,
+//         posY: 0,
+//         renderOrder: 0,
+//         manifest: {
+//             atlas: 'https://act-webstatic.hoyoverse.com/puzzle/hk4e/pz_I4DPYNZuP5/resource/puzzle/2024/01/25/ebc7ebb206d70ad9ce64375cafd3d582_1877002427940820416.atlas',
+//             img: [
+//                 {
+//                     src: 'https://act-webstatic.hoyoverse.com/puzzle/hk4e/pz_I4DPYNZuP5/resource/puzzle/2024/01/25/25354cba18689abaeaf57d5064f84f8c_8256955875500779931.png',
+//                     id: 'naxida',
+//                     type: 'image'
+//                 }
+//             ],
+//             json: 'https://act-webstatic.hoyoverse.com/puzzle/hk4e/pz_I4DPYNZuP5/resource/puzzle/2024/01/25/21487409d0820864e2b9a062c2b69b9c_4340573286094184063.json',
+//             animations: ['animation'],
+//             name: 'naxida',
+//             skeleton: {
+//                 hash: 'fa3szwC6z6A',
+//                 spine: '4.0.33',
+//                 x: -386.93,
+//                 y: -610.88,
+//                 width: 802.43,
+//                 height: 997.66
+//             }
+//         },
+//         maxVert: 6000,
+//         spineName: 'naxida',
+//         spineId: 'SwOky',
+//         animation: 'animation'
+//     },
+//     {
+//         visible: false,
+//         loop: true,
+//         scale: 0.85,
+//         posX: -230,
+//         posY: -100,
+//         renderOrder: 0,
+//         manifest: {
+//             atlas: 'https://act-webstatic.hoyoverse.com/puzzle/hk4e/pz_I4DPYNZuP5/resource/puzzle/2024/01/25/735271958c84b1d4e2fc1f4e9f1d5f00_1897303427965927200.atlas',
+//             img: [
+//                 {
+//                     src: 'https://act-webstatic.hoyoverse.com/puzzle/hk4e/pz_I4DPYNZuP5/resource/puzzle/2024/01/25/36555caa3409f5396554bf3ebc05d4fb_5302082293966880116.png',
+//                     id: 'bachong',
+//                     type: 'image'
+//                 }
+//             ],
+//             json: 'https://act-webstatic.hoyoverse.com/puzzle/hk4e/pz_I4DPYNZuP5/resource/puzzle/2024/01/25/6a8171399c28651ccc187bca09990ed6_5668672106837743986.json',
+//             animations: ['animation'],
+//             name: 'bachong',
+//             skeleton: {
+//                 hash: 'SSsWU0wfo1Y',
+//                 spine: '4.1.17',
+//                 x: -317.96,
+//                 y: -943.61,
+//                 width: 1185.55,
+//                 height: 1473.95
+//             }
+//         },
+//         maxVert: 2048,
+//         spineName: 'bachong',
+//         spineId: 'EfTzA',
+//         animation: 'animation'
+//     }
+// ]
+
+import manifest from './json/manifest.json'
+
+export { manifest }
+export const sceneData = {
+    id: 'scene_main',
+    children: [
+        {
+            name: 'xianyun',
+            uuid: 'xianyun',
+            renderOrder: 0,
+            position: [0, -33, 0],
+            scale: [0.85, 0.85, 0.85],
+            visible: false,
+            children: [],
+            type: 3,
+            spine: {
+                id: 'fJIMc',
+                defaultAnimation: 'animation',
+                maxVert: 8000
+            },
+            material: [
+                {
+                    id: 'SPINE'
+                }
+            ]
+        },
+        {
+            name: 'jaming',
+            uuid: 'jaming',
+            renderOrder: 0,
+            position: [90, 30, 0],
+            scale: [0.8, 0.8, 0.8],
+            visible: false,
+            children: [],
+            type: 3,
+            spine: {
+                id: 'GRzTk',
+                defaultAnimation: 'animation',
+                maxVert: 6000
+            },
+            material: [
+                {
+                    id: 'SPINE'
+                }
+            ]
+        },
+        {
+            name: 'xiao',
+            uuid: 'xiao',
+            renderOrder: 0,
+            position: [-50, 0, 0],
+            scale: [0.8, 0.8, 0.8],
+            visible: false,
+            children: [],
+            type: 3,
+            spine: {
+                id: '6GC5w',
+                defaultAnimation: 'animation',
+                maxVert: 6000
+            },
+            material: [
+                {
+                    id: 'SPINE'
+                }
+            ]
+        },
+        {
+            name: 'naxida',
+            uuid: 'naxida',
+            renderOrder: 0,
+            position: [0, 0, 0],
+            scale: [0.9, 0.9, 0.9],
+            visible: false,
+            children: [],
+            type: 3,
+            spine: {
+                id: 'SwOky',
+                defaultAnimation: 'animation',
+                maxVert: 6000
+            },
+            material: [
+                {
+                    id: 'SPINE'
+                }
+            ]
+        },
+        {
+            name: 'bachong',
+            uuid: 'bachong',
+            renderOrder: 0,
+            position: [-230, -100, 0],
+            scale: [0.85, 0.85, 0.85],
+            visible: false,
+            children: [],
+            type: 3,
+            spine: {
+                id: 'EfTzA',
+                defaultAnimation: 'animation',
+                maxVert: 2048
+            },
+            material: [
+                {
+                    id: 'SPINE'
+                }
+            ]
+        }
+    ]
+}
+
 const a = {
     default: {
         SPINE: {
